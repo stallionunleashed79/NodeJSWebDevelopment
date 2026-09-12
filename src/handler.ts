19481867bcaa2341d551d0bd600c63c5503ee3db
chasethesky79 <seshadri.bharath@yahoo.com>
2026-09-12 18:23:39 +0530

    Wrapped the code in async await

--- src/handler.ts
+++ src/handler.ts
@@ -1,14 +1,13 @@
 import { IncomingMessage, ServerResponse } from "http";
-import { readFile } from "fs";
+import { readFile } from "fs/promises";
 
-export const handler = (req: IncomingMessage, res: ServerResponse) => {
-  readFile("./data.json", "utf-8", (err: Error | null, data: string | Buffer) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Error reading data");
-      return;
-    }
+export const handler = async (req: IncomingMessage, res: ServerResponse) => {
+  try {
+    const data = await readFile("./data.json", "utf-8");
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(data as string, () => console.log("Response sent successfully."));
-  });
+    res.end(data);
+  } catch (err) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Internal Server Error");
+  }
 };
