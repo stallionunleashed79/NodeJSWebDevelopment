508fe9c215f0fe31cf9b4845eceb1aa9eb5325ad
chasethesky79 <seshadri.bharath@yahoo.com>
2026-09-21 00:17:03 +0530

    More changes

--- src/handler.ts
+++ src/handler.ts
@@ -3,6 +3,9 @@ import { readFile } from "fs/promises";
 
 export const handler = async (req: IncomingMessage, res: ServerResponse) => {
   try {
+    console.log(`Received request: ${req.method} ${req.url}, ${req.headers.host}, ${req.headers["user-agent"]}`);
+    const parsedUrl = new URL(req.url || "", `http://${req.headers.host}`);
+    console.log(`Parsed URL: ${parsedUrl.href}, Pathname: ${parsedUrl.pathname}, Protocol: ${parsedUrl.protocol}, Search Params: ${parsedUrl.searchParams}`);
     const data = await readFile("./data.json", "utf-8");
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(data);
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import { createServer } from "http";
 import { handler } from "./handler";
 
 const port = 3000;
-const server = createServer(handler);
+const server = createServer();
+server.on("request", handler);
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
