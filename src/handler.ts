cffc09ffe48f155fdd62c1bb5b3be4f810edd003
chasethesky79 <seshadri.bharath@yahoo.com>
2026-09-26 21:09:26 +0530

    More changes

--- src/handler.ts
+++ src/handler.ts
@@ -6,9 +6,18 @@ export const handler = async (req: IncomingMessage, res: ServerResponse) => {
     console.log(`Received request: ${req.method} ${req.url}, ${req.headers.host}, ${req.headers["user-agent"]}`);
     const parsedUrl = new URL(req.url || "", `http://${req.headers.host}`);
     console.log(`Parsed URL: ${parsedUrl.href}, Pathname: ${parsedUrl.pathname}, Protocol: ${parsedUrl.protocol}, Search Params: ${parsedUrl.searchParams}`);
-    const data = await readFile("./data.json", "utf-8");
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(data);
+    if (req.method !== "GET" || parsedUrl.pathname == "/favicon.ico") {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      if (!parsedUrl.searchParams.has('keyword')) {
+          res.write('Hello http');
+      } else {
+          res.write(`Hello ${parsedUrl.searchParams.get('keyword')}`);
+      }
+    }
+    res.end();
+    return;
   } catch (err) {
     res.writeHead(500, { "Content-Type": "text/plain" });
     res.end("Internal Server Error");
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import { createServer } from "http";
 import { handler } from "./handler";
+import { readFileSync } from "fs";
+import { createServer as createHttpsServer } from "https";
 
 const port = 3000;
-const server = createServer();
+const https_port = 3001;
+const server = createServer(handler);
 server.on("request", handler);
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+});
+const httpsConfig = {
+    key: readFileSync("key.pem"),
+    cert: readFileSync("cert.pem")
+};
+
+const httpsServer = createHttpsServer(httpsConfig, handler);
+httpsServer.listen(https_port, () => {
+  console.log(`HTTPS Server is running on https://localhost:${https_port}`);
 });
